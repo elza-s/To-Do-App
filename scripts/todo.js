6fff0f872e7f91b24d01421f4a769abb0831c97f
elza-s <elza_somu@yahoo.com>
2020-11-14 19:07:40 -0500

    added append function

--- scripts/todo.js
+++ scripts/todo.js
@@ -2,3 +2,26 @@ function sendAlert(message) {
 
   alert(message)
 }
+
+function addItem() {
+  var todo = document.getElementById("todo-item");
+
+  this.addToList(todo.value);
+
+  todo.value = null;
+}
+
+function addToList(message) {
+  var list = document.getElementById("todo-list");
+  var item = document.createElement("li");
+
+  item.appendChild(document.createTextNode(message));
+  list.appendChild(item);
+}
+
+var crossOut = document.querySelector("#todo-list");
+crossOut.addEventListener("click", strike);
+
+function strike(event) {
+  var strikeOut = document.createElement('del');
+}
